d7e71a0dd508ee239e4dc7496c40d5263de5d94b
Zernys <run.haakon@gmail.com>
2018-12-20 19:52:18 +0100

    Generalized main.js and added respawning

--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
-const roleHarvester = require('role.harvester');
+const _ = require('lodash');
+const roleModules = {
+    'harvester': require('role.harvester')
+};
 
 module.exports.loop = function() {
-    for (var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        switch (creep.memory.role) {
-            case 'harvester':
-                roleHarvester.run(creep);
-                break;
-            default:
-                break;
+
+    // Clear destroyed creeps from memory
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+        }
+    }
+
+
+    // Spawn creeps if targets are not met
+    const creepCounts = _.countBy(Game.creeps, creep => creep.memory.role);
+
+    for (var roleName in creepCounts) {
+        if (creepCounts[roleName] < Memory.creepTargetCounts[roleName]) {
+            let body = Memory.creepTypes[roleName].body;
+            let name = roleName + '_' + Game.time;
+            let memory = {role: roleName};
+
+            Game.spawns['Spawn1'].spawnCreep(body, name, memory);
+        }
+    }
+
+    
+    // Run the creeps
+    for (var creepName in Game.creeps) {
+        var creep = Game.creeps[creepName];
+        roleModules[creep.memory.role].run(creep);
         }
     }
 }
