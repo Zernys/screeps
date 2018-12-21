5b5615a99460c0e5e62b366c53024ea580a91276
Zernys <run.haakon@gmail.com>
2018-12-21 04:37:36 +0100

    Bug fix

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ module.exports.loop = function() {
 
     // Spawn creeps if targets are not met
     const creepCounts = _.countBy(Game.creeps, creep => creep.memory.role);
-    for(let i = 0; i++; i<Memory.roleNames.length) {
+    for(let i = 0; i<Memory.roleNames.length; i++) {
         let roleName = Memory.roleNames[i];
         if((!creepCounts.hasOwnProperty(roleName) && Memory.creepTypes[roleName].targetCount > 0) ||
             (creepCounts.hasOwnProperty(roleName) && creepCounts[roleName] < Memory.creepTypes[roleName].targetCount)) {
