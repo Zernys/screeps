e0a1fae6f24f57006451a94c47543140ec3945bf
Zernys <run.haakon@gmail.com>
2018-12-21 23:13:11 +0100

    Added mode state machine to harvester

--- src/harvester.js
+++ src/harvester.js
@@ -8,16 +8,52 @@ var Harvester = function(creep) {
 Harvester.prototype = Object.create(Role.prototype);
 Harvester.prototype.constructor = Harvester;
 
+Harvester.prototype.init = function() {
+  this.creep.memory.mode = 'harvest';
+};
+
 Harvester.prototype.tick = function() {
     var creep = this.creep;
 
-    if(creep.carry.energy < creep.carryCapacity) {
-        this.harvest();
-    }else{
-        hasStore = this.deposit();
-        if (!hasStore) {
+    switch(creep.memory.mode) {
+        case 'harvest':
+            this.harvest();
+            if(creep.carry.energy === creep.carryCapacity) {
+                creep.memory.mode = 'deposit';
+            }
+            break;
+
+        case 'deposit':
+            var hasWork = this.deposit();
+            if(!hasWork) {
+                if(creep.carry.energy < creep.carryCapacity) {
+                    creep.memory.mode = 'harvest';
+                } else {
+                    creep.memory.mode = 'idle';
+                }
+            }
+            if(creep.carry.energy === 0) {
+                creep.memory.mode = 'harvest'
+            }
+            break;
+
+        case 'idle':
             this.idle();
-        }
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType === STRUCTURE_SPAWN ||
+                        structure.structureType === STRUCTURE_EXTENSION) && structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                this.creep.memory.mode = 'deposit';
+            }
+            break;
+
+        default:
+            console.log(creep.name + ": Unsupported mode " + creep.memory.mode + ", resetting...");
+            creep.memory.mode = 'harvest';
+            break;
     }
 };
 
