9840ca6d14e5c723533da25163126d456981ae4b
Zernys <run.haakon@gmail.com>
2018-12-21 02:02:29 +0100

    Bug fix

--- src/role.js
+++ src/role.js
@@ -9,7 +9,7 @@ Role.prototype.getRole = function() {
 };
 
 Role.prototype.harvest = function() {
-    const creep = self.creep;
+    const creep = this.creep;
     let sources = creep.room.find(FIND_SOURCES);
     if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
         creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
