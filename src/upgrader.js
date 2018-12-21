dc4dcecf391d87775406e31dadb4ba60d63b2b57
Zernys <run.haakon@gmail.com>
2018-12-21 20:05:24 +0100

    Added mode to upgrader

--- src/upgrader.js
+++ src/upgrader.js
@@ -8,13 +8,30 @@ var Upgrader = function(creep) {
 Upgrader.prototype = Object.create(Role.prototype);
 Upgrader.prototype.constructor = Upgrader;
 
+Upgrader.prototype.init = function() {
+    this.creep.memory.mode = 'harvest';
+};
+
 Upgrader.prototype.tick = function() {
     var creep = this.creep;
 
-    if(creep.carry.energy === 0) {
-        this.harvest();
-    }else{
-        this.upgrade();
+    switch(creep.memory.mode) {
+        case 'harvest':
+            this.harvest();
+            if(creep.carry.energy === creep.carryCapacity) {
+                creep.memory.mode = 'upgrade';
+            }
+            break;
+        case 'upgrade':
+            this.upgrade();
+            if(creep.carry.energy === 0) {
+                creep.memory.mode = 'harvest';
+            }
+            break;
+        default:
+            console.log(creep.name + ": Unsupported mode " + creep.memory.mode + ", resetting...");
+            creep.memory.mode = 'harvest';
+            break;
     }
 };
 
