0ee06f7561388854acf94ccf161fc4117428986d
Zernys <run.haakon@gmail.com>
2018-12-23 02:40:48 +0100

    Made harvesters prioritize target with least energy

--- src/harvester.js
+++ src/harvester.js
@@ -68,6 +68,7 @@ Harvester.prototype.deposit = function() {
                 structure.structureType === STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
         }
     });
+    targets.sort((a, b) => a.energy - b.energy);
     if(targets.length > 0) {
         if(creep.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
             creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
